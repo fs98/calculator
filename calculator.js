ce39b77e23da490700652be0970302ed0289d1ee
Fata Sefer <sefer.fata@gmail.com>
2022-12-07 16:34:36 +0100

    feat: create four basic operations functions

---
+++ calculator.js
@@ -0,0 +1,15 @@
+const add = (valueA, valueB) => {
+  return valueA + valueB;
+};
+
+const substract = (valueA, valueB) => {
+  return valueA - valueB;
+};
+
+const multiply = (valueA, valueB) => {
+  return valueA * valueB;
+};
+
+const divide = (valueA, valueB) => {
+  return valueA / valueB;
+};
