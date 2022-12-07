b6551339a59836266d8d5f7eba80ac3517090811
Fata Sefer <sefer.fata@gmail.com>
2022-12-07 16:37:43 +0100

    feat: create operate function

--- calculator.js
+++ calculator.js
@@ -13,3 +13,22 @@ const multiply = (valueA, valueB) => {
 const divide = (valueA, valueB) => {
   return valueA / valueB;
 };
+
+const operate = (operator, valueA, valueB) => {
+  switch (operator) {
+    case "+":
+      return add(valueA, valueB);
+
+    case "-":
+      return substract(valueA, valueB);
+
+    case "*":
+      return multiply(valueA, valueB);
+
+    case "/":
+      return divide(valueA, valueB);
+
+    default:
+      break;
+  }
+};
