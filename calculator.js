852273cc6782054b2146276fb93f188a4fba9323
Fata Sefer <sefer.fata@gmail.com>
2022-12-13 23:01:52 +0100

    write round function to round every result to 6 decimals

--- calculator.js
+++ calculator.js
@@ -1,19 +1,23 @@
+const round = (value) => {
+  return parseFloat(Math.round(value + "e6") + "e-6");
+};
+
 const operate = (valueA, valueB, operator) => {
   switch (operator) {
     case "+":
-      return valueA + valueB;
+      return round(valueA + valueB);
 
     case "-":
-      return valueA - valueB;
+      return round(valueA - valueB);
 
     case "*":
-      return valueA * valueB;
+      return round(valueA * valueB);
 
     case "/":
       if (valueB === 0) {
         return "nope";
       }
-      return valueA / valueB;
+      return round(valueA / valueB);
 
     default:
       break;
