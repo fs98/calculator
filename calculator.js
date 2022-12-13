b6a571006961033e963b39bc270fd9858da14f44
Fata Sefer <sefer.fata@gmail.com>
2022-12-13 23:08:01 +0100

    define variable and write updateDisplay self invoking function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,13 @@
+let displayValue = 0;
+let valueA = null;
+let valueB = null;
+let operator = null;
+let result = null;
+
+const updateDisplay = (() => {
+  document.getElementById("display").innerText = displayValue;
+})();
+
 const round = (value) => {
   return parseFloat(Math.round(value + "e6") + "e-6");
 };
