81387ec9e15652837806bc44dde3b6ae09f7a130
Fata Sefer <sefer.fata@gmail.com>
2022-12-19 21:11:21 +0100

    feat: start writing calculator class

---
+++ oop-calculator.js
@@ -0,0 +1,57 @@
+class Calculator {
+  constructor(displayValue, valueA, valueB, operator, result) {
+    this.displayValue = displayValue;
+    this.valueA = valueA;
+    this.valueB = valueB;
+    this.operator = operator;
+    this.result = result;
+  }
+
+  roundResult = (value) => {
+    return parseFloat(Math.round(value + "e6") + "e-6").toString();
+  };
+
+  operate = (valueA, valueB, operator) => {
+    valueA = Number(valueA);
+    valueB = Number(valueB);
+
+    switch (operator) {
+      case "+":
+        return this.roundResult(valueA + valueB);
+
+      case "-":
+        return this.roundResult(valueA - valueB);
+
+      case "*":
+        return this.roundResult(valueA * valueB);
+
+      case "/":
+        if (valueB === 0) {
+          return "nope";
+        }
+        return this.roundResult(valueA / valueB);
+    }
+  };
+
+  handleResult = () => {
+    const result = this.operate(this.valueA, this.valueB, this.operator);
+    this.displayValue = result;
+
+    if (result !== "nope") {
+      this.valueA = result;
+      this.valueB = null;
+      this.operator = null;
+      this.result = null;
+    }
+  };
+}
+
+const calculator = new Calculator();
+
+calculator.valueA = "4";
+calculator.valueB = "10";
+calculator.operator = "*";
+
+calculator.handleResult();
+
+console.log("After operating", calculator.valueA);
